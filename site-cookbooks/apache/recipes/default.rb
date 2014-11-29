a11c934d4972c9fa74a500e6da93e443e2ba3186
yozaru <snowdropyo@gmail.com>
2014-11-29 10:29:00 +0900

    add apache template

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -13,3 +13,12 @@ end
 service "httpd" do
  action [ :enable, :start ]
 end
+
+template "httpd.conf" do
+ path "/etc/httpd/conf/httpd.conf"
+ owner "root"
+ group "root"
+ mode 0644
+ notifies :reload, 'service[httpd]'
+end
+
