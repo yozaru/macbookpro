916ba653d9b341c04e8f667ee78ca70866a522fb
yozaru <snowdropyo@gmail.com>
2014-12-14 16:59:58 +0900

    test

--- ruby-study/test.rb
+++ ruby-study/test.rb
@@ -12,13 +12,13 @@ class Yo
  end
 end
 
-yo = Yo.new
-puts "@洋のパラメータ"
-puts "-------------------------"
-puts "名前:" + yo.name
-puts "体力:" + yo.hitpoint.to_s
-puts "攻撃力:" + yo.attack.to_s
-puts "-------------------------"
+#yo = Yo.new
+#puts "@洋のパラメータ"
+#puts "-------------------------"
+#puts "名前:" + yo.name
+#puts "体力:" + yo.hitpoint.to_s
+#puts "攻撃力:" + yo.attack.to_s
+#puts "-------------------------"
 
 class Aya
  attr_accessor :name, :hitpoint, :attack
@@ -34,14 +34,25 @@ class Aya
  end
 end
 
-aya = Aya.new
-puts "@アヤのパラメータ"
-puts "-------------------------"
-puts "名前:" + aya.name
-puts "体力:" + aya.hitpoint.to_s
-puts "攻撃力:" + aya.attack.to_s
-puts "-------------------------"
+#aya = Aya.new
+#puts "@アヤのパラメータ"
+#puts "-------------------------"
+#puts "名前:" + aya.name
+#puts "体力:" + aya.hitpoint.to_s
+#puts "攻撃力:" + aya.attack.to_s
+#puts "-------------------------"
 
 #Display Class
 class Display
+ def initialize(yo,aya)
+  @yo = yo
+  @aya = aya 
+ end
+
+ def parameter
+  puts @yo.name
+ end
 end
+
+class = Class.new
+
