c4a423dabcb0e1db5849beb1fb830dbbd72b0e73
maruyama_yo <maruyama_yo@applibot.co.jp>
2014-12-15 18:41:58 +0900

    add 1215office

--- ruby-study/test.rb
+++ ruby-study/test.rb
@@ -8,7 +8,7 @@ class Yo
  end
 
  def dead?
-  @hp <=0
+  @hitpoint <= 0
  end
 end
 
@@ -22,7 +22,7 @@ class Aya
  end
 
  def dead?
-  @hp <=0
+  @hitpoint <=0
  end
 end
 
@@ -53,23 +53,57 @@ puts display.parameter
 #戦闘クラス
 class Battle
 
- def initialize(yo, aya)
-    @yo = yo
-    @aya = aya
+ def initialize(yo,aya)
+    @yo = Yo.new
+    @aya = Aya.new
  end
  
  def attack
-  damege = 3
-    puts yo.name + "の攻撃！！ %iのダメージ" % damage
-    yo.hitpoint = yo.hitpoint - damage
-
-    puts aya.name + "の攻撃！！ %iのダメージ" % damage
-    aya.hitpoint = aya.hitpoint - damage
+  damage1 = [*1..10].sample
+    puts @yo.name + "の攻撃！！ %iのダメージ" % damage1
+    @yo.hitpoint = @yo.hitpoint - damage1
+  
+  damage2 = [*1..10].sample
+    puts @aya.name + "の攻撃！！ %iのダメージ" % damage2
+    @aya.hitpoint = @aya.hitpoint - damage2
     puts ""
 
     @yo.dead? or @aya.dead? # どちらかのキャラクターの hp が無くなったかの判定
     end
+
+ def judge
+  if @yo.dead? and @aya.dead? #Draw
+   puts "DRAW!!!!!!"
+  elseif @aya.dead?
+   puts "Yo Win!!!!!!"
+  elseif @yo.dead?
+   puts "Aya Win!!!!!"
+  end 
+ end
 end 
 
-battle = Battle.new
-battle.attack
+
+#ゲームクラス
+class Game
+ #ゲーム開始
+ def start
+  yo = Yo.new
+  aya = Aya.new
+  battle = Battle.new(yo,aya)
+  while yo.hitpoint >= 0 or @aya.hitpoint >= 0 do
+  puts "###########################"
+  puts "戦闘開始！"
+  puts "洋 HP:%i, <-> あや HP:%i" % [yo.hitpoint, aya.hitpoint]
+  print "ENTERキーを押下すると実行, 3を入力すると終了 > "
+  input = gets.chomp.to_i
+   if (input == 3)
+        exit
+   end
+   descision = battle.attack
+  end
+  battle.judge 
+ end
+
+end
+game=Game.new
+game.start
